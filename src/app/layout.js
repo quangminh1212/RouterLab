0510c98a2dcb8917eb66a9864ea98e33e247eb07
Bach Minh Quang <0947556012abc@gmail.com>
2026-05-07 23:44:10 +0700

    fix: self-host Material Symbols font to avoid CDN blocking

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,13 +55,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         <ThemeProvider>
@@ -73,4 +66,5 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
 
