dd1152380f14b17ac0dce3a9ea40f9713d7a4543
Bach Minh Quang <0947556012abc@gmail.com>
2026-05-07 20:13:47 +0700

    perf: add lazy loading for 6 additional large modals

--- src/shared/components/optimized/LazyModals.js
+++ src/shared/components/optimized/LazyModals.js
@@ -38,6 +38,36 @@ const DynamicModelSelectModal = dynamic(() => import("../ModelSelectModal"), {
   loading: () => null,
 });
 
+const DynamicEditConnectionModal = dynamic(() => import("../EditConnectionModal"), {
+  ssr: false,
+  loading: () => null,
+});
+
+const DynamicRamConfigModal = dynamic(() => import("../RamConfigModal"), {
+  ssr: false,
+  loading: () => null,
+});
+
+const DynamicKiroSocialOAuthModal = dynamic(() => import("../KiroSocialOAuthModal"), {
+  ssr: false,
+  loading: () => null,
+});
+
+const DynamicAddCustomEmbeddingModal = dynamic(() => import("../AddCustomEmbeddingModal"), {
+  ssr: false,
+  loading: () => null,
+});
+
+const DynamicNineRemotePromoModal = dynamic(() => import("../NineRemotePromoModal"), {
+  ssr: false,
+  loading: () => null,
+});
+
+const DynamicIFlowCookieModal = dynamic(() => import("../IFlowCookieModal"), {
+  ssr: false,
+  loading: () => null,
+});
+
 export const LazyChangelogModal = memo(DynamicChangelogModal);
 export const LazyOAuthModal = memo(DynamicOAuthModal);
 export const LazyCursorAuthModal = memo(DynamicCursorAuthModal);
@@ -45,3 +75,9 @@ export const LazyKiroAuthModal = memo(DynamicKiroAuthModal);
 export const LazyGitLabAuthModal = memo(DynamicGitLabAuthModal);
 export const LazyPricingModal = memo(DynamicPricingModal);
 export const LazyModelSelectModal = memo(DynamicModelSelectModal);
+export const LazyEditConnectionModal = memo(DynamicEditConnectionModal);
+export const LazyRamConfigModal = memo(DynamicRamConfigModal);
+export const LazyKiroSocialOAuthModal = memo(DynamicKiroSocialOAuthModal);
+export const LazyAddCustomEmbeddingModal = memo(DynamicAddCustomEmbeddingModal);
+export const LazyNineRemotePromoModal = memo(DynamicNineRemotePromoModal);
+export const LazyIFlowCookieModal = memo(DynamicIFlowCookieModal);
