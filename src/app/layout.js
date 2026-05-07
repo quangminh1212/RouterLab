8b32e095a644483ad2b4f94035540a6242abb575
Bach Minh Quang <0947556012abc@gmail.com>
2026-05-07 20:02:41 +0700

    perf: optimize font loading with display swap strategy

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+﻿import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/shared/components/ThemeProvider";
 import { initConsoleLogCapture } from "@/lib/consoleLogBuffer";
@@ -32,6 +32,9 @@ bootstrapServerInits();
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
+  display: "swap",
+  preload: true,
+  fallback: ["system-ui", "arial"],
 });
 
 export const metadata = {
@@ -54,10 +57,12 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
           rel="stylesheet"
+          media="print"
+          onLoad="this.media=''all''"
         />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
